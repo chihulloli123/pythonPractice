0103de3fef9a2380951d72c5dc9981ad71f03c9f
amandaPerez <chihulloli123@gmail.com>
2026-01-12 23:01:24 +0000

    showed icebox codespaces/shortcutes

--- reps/arrays_basics.py
+++ reps/arrays_basics.py
@@ -2,7 +2,7 @@
 #Goal: get acclimate to iterating, checking, and collecting
 
 
-nums = [1,2,3,4,5]
+#nums = [1,2,3,4,5]
 
 #for n in nums:
 #    print(n)
@@ -28,3 +28,108 @@ nums = [1,2,3,4,5]
 #    print("YES")
 #else:
 #    print("No")
+
+#l = 2
+#r = 5
+
+#odds = []
+
+#for n in range(l, r + 1):
+#    if n % 2 == 1:
+#        odds.append(n)
+    
+#print(odds)
+
+#nums = [1, 2, 3, 2, 4, 2]
+#k = 2
+
+#count = 0
+
+#for n in nums:
+#    if n == k:
+#        count += 1
+    
+#print(count)
+
+#def count_occurences(arr, k):
+#    count = 0
+
+#    for n in arr:
+#        if n == k:
+#            count += 1
+
+#    return count
+
+#print(count_occurences([1, 2, 3, 2, 4, 2], 2))
+
+#nums = [1, 2, 3, 2, 4, 2]
+#k = 2
+
+#indices = []
+
+#for i in range(len(nums)):
+#    if nums[i] == k:
+#        indices.append(i)
+    
+#print(indices)
+
+#nums = [4, 1, 2, 3, 2]
+#k = 2
+
+#for i in range(len(nums)):
+#    if nums[i] == k:
+#        print(i)
+#        break
+
+#nums = [4, 1, 2, 3, 2]
+#k = 2
+
+#def first_occurence(nums, k):
+#    for i in range(len(nums)):
+#        if nums[i] == k:
+#            return i 
+#    return -1
+
+#print(first_occurence(nums, k))
+
+#nums = [1, 5, 2, 8, 3]
+#t = 3
+
+#for n in nums:
+#    if n > t:
+#        print(n)
+
+
+#nums = [1, 5, 2, 8, 3]
+#t = 3
+
+#def filter_greater(nums, t):
+#    result = []
+
+
+
+#    for n in nums:
+#        if n > t:
+#            result.append(n)
+
+#    return result
+
+#print(filter_greater([1, 5, 2, 8, 3], 3))
+
+
+#def solve(nums, t):
+#    result = []
+
+#   for n in nums:
+#        if n > t:
+#            result.append(n)
+#    return result
+
+#nums = [1, 5, 2, 8, 3]
+#t = 3
+
+#output = solve(nums, t)
+#print(output) 
+
+
+
